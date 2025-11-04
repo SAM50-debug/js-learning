b2760e9579b62803c6c47620b5854cf429a327a5
SAM50-debug <sanyamkumar696@gmail.com>
2025-11-04 17:12:24 +0000

    learn

---
+++ 01_basic/variable.js
@@ -0,0 +1,6 @@
+const account_id = 14453
+let account_email = "sam@gamil.com"
+var account_password = "12345"
+account_city = "Ludhiana"
+
+console.table([account_id,account_email,account_password,account_city])
