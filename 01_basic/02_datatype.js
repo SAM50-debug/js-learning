7f7ceeaf226ee4e2ce111a09d5793b7f2647b7f2
SAM50-debug <sanyamkumar696@gmail.com>
2025-11-05 06:51:32 +0000

    update

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//alert("hello")
+let naem = "sam"
+let age = 19
+let isLoggedIn = false
+
+console.table([naem,age,isLoggedIn])
---
+++ 01_basic/03_variable.js
@@ -0,0 +1,9 @@
+let val = 3
+let negval = -val
+//console.log(negval)
+
+//let str1 = "sam"
+//let str2 = " k"
+//console.log(str1+str2);
+
+console.log(11)
